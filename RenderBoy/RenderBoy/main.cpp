ea08aceaf337f9d095c3d21caf8b6c99dbccd239
SSPYR0 <jarronblizzard@gmail.com>
2020-02-23 20:06:19 -0500

    basic setup

---
+++ RenderBoy/RenderBoy/main.cpp
@@ -0,0 +1,14 @@
+#include <Eigen/Dense>
+#include <iostream>
+
+using namespace Eigen;
+
+int main() {
+
+	char a;
+	Vector2i v1 = Vector2i(2, 1);
+	std::cout << v1(0) << std::endl;
+
+	std::cin >> a;
+
+}
